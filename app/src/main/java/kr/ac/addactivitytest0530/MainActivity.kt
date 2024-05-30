4f1ba03d048b5d934c76b39ff363ca63f3926651
shinjaesang <tlswltkd1011@gmail.com>
2024-05-30 16:26:54 +0900

    직접풀어보기 10-1

--- app/src/main/java/kr/ac/addactivitytest0530/MainActivity.kt
+++ app/src/main/java/kr/ac/addactivitytest0530/MainActivity.kt
@@ -4,15 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
+    lateinit var rg : RadioGroup
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rg = findViewById<RadioGroup>(R.id.rg)
+        rg.check(R.id.radiosecond)
 
         var btnMain = findViewById<Button>(R.id.btnMain)
-        btnMain.setOnClickListener {
+        var intent = null
+        rg.setOnClickListener {
             var intent = Intent(applicationContext, SecondActivity::class.java)
+            when(rg.checkedRadioButtonId){
+                R.id.radiosecond -> intent = Intent(applicationContext, SecondActivity::class.java)
+                R.id.radiothird -> intent = Intent(applicationContext, ThirdActivity::class.java)
+            }
             startActivity(intent)
 
 
